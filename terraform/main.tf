28eb8dde125b58560d766ae334db4a4f908f5a02
jatin bhalla <jatinbhalla18@gmail.com>
2023-02-16 09:02:15 +0530

    assignments.txt

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,33 +1,18 @@
-terraform {
-
-   required_version = ">=0.12"
-
-   required_providers {
-     azurerm = {
-       source = "hashicorp/azurerm"
-       version = "~>2.0"
-     }
-   }
- }
-
- provider "azurerm" {
-   features {}
- }
 
  resource "azurerm_resource_group" "test" {
-   name     = "acctestrg"
-   location = "West US 2"
+   name     = var.resource_group_name
+   location = var.region
  }
 
  resource "azurerm_virtual_network" "test" {
-   name                = "acctvn"
+   name                = var.virtual_network_name
    address_space       = ["10.0.0.0/16"]
    location            = azurerm_resource_group.test.location
    resource_group_name = azurerm_resource_group.test.name
  }
 
  resource "azurerm_subnet" "test" {
-   name                 = "acctsub"
+   name                 = var.azurerm_subnet_name
    resource_group_name  = azurerm_resource_group.test.name
    virtual_network_name = azurerm_virtual_network.test.name
    address_prefixes     = ["10.0.2.0/24"]
