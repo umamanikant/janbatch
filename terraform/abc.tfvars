65b36e49b3238d29b8d0db82a1ba1bbb422050e5
jatin bhalla <jatinbhalla18@gmail.com>
2023-02-16 09:01:25 +0530

    assignment

---
+++ terraform/abc.tfvars
@@ -0,0 +1,4 @@
+resource_group_name = "jatinbhalla"
+region              = "North Europe"
+virtual_network_name        = "acctvn"
+azurerm_subnet_name         = "acctsub"
---
+++ terraform/variables.tf
@@ -0,0 +1,15 @@
+variable "azurerm_subnet_name" {
+  type = string
+}
+
+variable "virtual_network_name" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
+variable "resource_group_name" {
+  type        = string
+  description = "name of resource group"
+}
